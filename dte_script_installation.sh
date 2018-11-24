fd4767dfb83fd4921568c9de9228590068f2851d
Oscar F. Carcamo Oyarzo <35848099+VKYC@users.noreply.github.com>
2018-11-24 00:07:21 -0300

    Add files via upload

---
+++ dte_script_installation.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+#Install these dependencies:
+
+aptitude install python3-pip
+pip3 install xmltodict dicttoxml xlrd pdf417gen cchardet signxml pysftp num2words
+
+#Install these modules:
+
+#l10n_cl_chart_of_account
+#l10n_cl_dte_point_of_sale
+#l10n_cl_fe
+#l10n_cl_stock_picking
+#report_xlsx
